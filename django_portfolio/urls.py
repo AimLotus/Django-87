903039e13b9bc7b57fff9780630778f73530b2d1
ErickRodriguez <AimLotus@hotmail.com>
2023-09-14 00:31:54 -0500

    xds

--- django_portfolio/urls.py
+++ django_portfolio/urls.py
@@ -23,6 +23,7 @@ from portfolio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('render.urls')),
     path('', views.home, name='home'),
     path('blog/', include('blog.urls'))
 ]
